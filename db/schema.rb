ecfb496e3c604db1fb471ae00b4199658d0dcb13
Michelly <michelly.silva753@gmail.com>
2021-10-12 19:04:57 -0300

    feature: add Markers controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_200042) do
+ActiveRecord::Schema.define(version: 2021_10_12_205609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "markers", force: :cascade do |t|
+    t.string "type"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.string "obs"
+    t.string "created_by"
+    t.integer "upvotes"
+    t.integer "downvotes"
+    t.boolean "verified"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
