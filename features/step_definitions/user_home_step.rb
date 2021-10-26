316ee4a1257ae60c3e0942e5be71b3a8ccb68b53
Vitor Hugo Brasiliense da Rosa <53880209+vitor-hbr@users.noreply.github.com>
2021-10-26 20:46:36 -0300

    fix: Login system

--- features/step_definitions/user_home_step.rb
+++ features/step_definitions/user_home_step.rb
@@ -3,12 +3,12 @@ Dado('que estou na página home') do
 end
 
 Quando('a página é carregada') do
-  uri = URI.parse('https://maps.googleapis.com/maps/api/js?key='+Rails.application.credentials.google_maps_api_key)
+  uri = URI.parse('https://maps.googleapis.com/maps/api/js?key=AIzaSyBLCFpYycOKx17cjeZeBltPQ4WK792ms9A')
   response = Net::HTTP.get_response(uri)
 end
 
 Então('o mapa deve ser exibido') do
-  uri = URI.parse('https://maps.googleapis.com/maps/api/js?key='+Rails.application.credentials.google_maps_api_key)
+  uri = URI.parse('https://maps.googleapis.com/maps/api/js?key=AIzaSyBLCFpYycOKx17cjeZeBltPQ4WK792ms9A')
   response = Net::HTTP.get_response(uri)
   expect(response).to be_kind_of(Net::HTTPSuccess)
 end
