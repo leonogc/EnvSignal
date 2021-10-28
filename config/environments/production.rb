0e0a9c8b35287fbe81c79491a7e5c3a9d0c68d73
Vitor Hugo Brasiliense da Rosa <53880209+vitor-hbr@users.noreply.github.com>
2021-10-28 01:09:39 -0300

    chore: Heroku ES6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "myapp_#{Rails.env}"
