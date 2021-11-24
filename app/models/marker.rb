ec1b3524b260ca146a47d92eb0a27e0e818a4f8e
ThewMax <123mateus123@gmail.com>
2021-11-24 19:32:58 -0300

    feat: Implemented severity with cucumber tests and started edit marker testing

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -3,6 +3,7 @@ class Marker < ApplicationRecord
     validates :disaster_type, presence: {message: "É necessário informar o tipo de desastre corrente!"}
     validates :latitude, presence: {message: "É obrigatório informar a Latitude!"}
     validates :longitude, presence: {message: "É obrigatório informar a Longitude!"}
+    validates :severity, absence: {message: "The marker is not verified!"}, unless: -> { verified? }
 
     def upvotes
         Voter.where(marker_id: self.id, vote: :upvote).length()
@@ -16,4 +17,6 @@ class Marker < ApplicationRecord
     scope :own_user, -> (userId) { where('user_id = ?', userId) }
     scope :pending_marker, -> (userId) { where('verified != ? AND user_id != ?', true, userId)  }
     scope :verified_marker, -> (userId) { where('verified = ? AND user_id != ?', true, userId) }
+
+    enum severity: {I: 1, II: 2, III: 3, IV: 4}
 end
